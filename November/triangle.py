4d3390a8647199c90348688b6f7256dbbf86c570
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2022-11-20 21:11:07 +0100

    codewars: triangle array not working yet

--- November/triangle.py
+++ November/triangle.py
@@ -1,5 +1,16 @@
+import array
+
 def row_sum_odd_numbers(n):
-    for i in range(n):
-        print(i)
+    arr = []
+    # number will be "appended" if odd
+    number = 1
+    # increase until n then stop
+    become_n = 0
+    while become_n != n:
+        # try out list comprehension
+        arr = [number if number % 2 == 1 else None for i in range(become_n)]
+        number += 1
+        become_n += 1
+    print(arr)
 
 row_sum_odd_numbers(10)
