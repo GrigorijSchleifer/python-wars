5568aad690371fb6f1555179c2e91f4ac1f15a8e
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2022-11-23 06:27:12 +0100

    codewors: triangle kata, still struggling

--- November/triangle.py
+++ November/triangle.py
@@ -1,16 +1,30 @@
-import array
+import numpy as np
 
 def row_sum_odd_numbers(n):
-    arr = []
-    # number will be "appended" if odd
-    number = 1
-    # increase until n then stop
-    become_n = 0
-    while become_n != n:
-        # try out list comprehension
-        arr = [number if number % 2 == 1 else None for i in range(become_n)]
+    number = 0
+    len_arr = 1
+    arr_outer = []
+    arr_inner = []
+    for arr in arr_outer:
         number += 1
-        become_n += 1
-    print(arr)
+        if number % 2 == 1 and len(arr) < len_arr:
+            arr_outer.append(arr_inner.append(number))
+            len_arr += 1 
+    print(arr_outer)
 
 row_sum_odd_numbers(10)
+
+arr1 = [1, 2, 3] 
+arr2 = [4, 5, 6] 
+arr3 = (arr2)
+print(arr3)
+
+for i in range(10):
+    number = 0
+    len_arr = 1
+    arr_outer = []
+    arr_inner = []
+    
+    if i % 2 == 1:
+        arr_outer.append(arr_inner.append(number))
+        print(i)
