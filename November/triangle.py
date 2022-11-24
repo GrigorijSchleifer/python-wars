2823935b2f35c671f034f54a1e03403b1bb6885e
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2022-11-24 11:05:01 +0100

    codewars: triangel kata, struggling, pyenv added

--- November/triangle.py
+++ November/triangle.py
@@ -7,24 +7,24 @@ def row_sum_odd_numbers(n):
     arr_inner = []
     for arr in arr_outer:
         number += 1
-        if number % 2 == 1 and len(arr) < len_arr:
-            arr_outer.append(arr_inner.append(number))
+        if number % 2 == 1:
+            arr_outer.append(arr_inner.extend(number))
             len_arr += 1 
     print(arr_outer)
 
 row_sum_odd_numbers(10)
 
-arr1 = [1, 2, 3] 
-arr2 = [4, 5, 6] 
-arr3 = (arr2)
-print(arr3)
+number = 0
+len_arr = 1
+arr_outer = []
+arr_inner = []
+for i in range(10):
+    number += 1
+    if number % 2 != 1 and i < 10:
+        arr_outer.append(arr_inner.extend(number))
+        len_arr += 1 
+    print(arr_outer)
 
 for i in range(10):
-    number = 0
-    len_arr = 1
-    arr_outer = []
-    arr_inner = []
-    
-    if i % 2 == 1:
-        arr_outer.append(arr_inner.append(number))
-        print(i)
+    arr_inner.extend(i)
+    print(i)
