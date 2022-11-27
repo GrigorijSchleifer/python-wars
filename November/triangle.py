20c2abfe7f633a34e9bf847471046834e1ed12e9
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2022-11-27 13:50:24 +0100

    codewars: trangle kata, still struggling , new idea to create a filled list of lists first and iter through them

--- November/triangle.py
+++ November/triangle.py
@@ -9,19 +9,29 @@ Fill line with odd numbers until length is one longer than the line before
 Insert this filled list on next location
 '''
 
-number = 1
+number = 0
 
 for i in range(10):
     print(f'i is {i}')
+    number += 1
+    print(f'number is {number}')
     if number % 2 > 0:
         print(f'number is {number}')
         arr_inner.append(number) 
         print(f'arr_inner is {arr_inner} and its length is {len(arr_inner)}')
-        if len(arr_inner) == i + 1:
+        if len(arr_inner) == i:
             arr_outer.insert(i, arr_inner)
             arr_inner = []
             print(f'arr_outer is {arr_outer}')
-    number += 1
     
 
 print(arr_outer)
+
+def triange_row_sum(n):
+    list_with_lists = [[]] * n
+    print(list_with_lists)
+    for idx, nr in enumerate(list_with_lists):
+        print(list_with_lists[idx]) 
+
+triange_row_sum(10)
+
